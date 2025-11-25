63bc7c15ebd8f0d88826cd80bf024488f57de367
669yash <yash.mali@mitaoe.ac.in>
2025-11-25 23:39:18 +0530

    first commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -294,7 +294,7 @@ export default function Hero() {
 
       {/* Scroll Indicator */}
       <motion.div
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-20"
+        className="hidden sm:block absolute bottom-6 md:bottom-8 left-1/2 transform -translate-x-1/2 z-10 pointer-events-none md:pointer-events-auto"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 2 }}
